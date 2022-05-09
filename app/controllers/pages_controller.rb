6a34a711be1b3c0648d8277e0ce6cad80f598e2c
Yan Siegers <hello@yansiegers.nl>
2022-05-09 18:56:52 +0200

    Add chartkick

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,6 @@ class PagesController < ApplicationController
   def home
     @properties = MeasuringPoint.pluck(:property).uniq
     @measuring_points = MeasuringPoint.where(property: params[:filter])
+    @data = @measuring_points.pluck(:timestamp, :value)
   end
 end
