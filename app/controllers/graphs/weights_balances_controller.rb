e612a4f0c5769b5d91091f67639392c0eb124601
Yan Siegers <hello@yansiegers.nl>
2022-05-12 14:19:42 +0200

    Add Plotly via script

--- app/controllers/graphs/weights_balances_controller.rb
+++ app/controllers/graphs/weights_balances_controller.rb
@@ -3,6 +3,9 @@
 module Graphs
   # Controls weights and balances
   class WeightsBalancesController < ApplicationController
-    def index; end
+    def index
+      @measuring_points = MeasuringPoint.where(property: 'COG_dev_y')
+      @data = @measuring_points.pluck(:timestamp, :value)
+    end
   end
 end
