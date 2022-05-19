e7d69db98d5d98d24bc2885cf2d0aee58e02e768
Yan Siegers <hello@yansiegers.nl>
2022-05-19 15:39:13 +0200

    Add creation of graphs

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -4,4 +4,26 @@ class GraphsController < ApplicationController
   def index
     @graphs = Graph.all
   end
+
+  def new
+    @graph = Graph.new
+  end
+
+  def create
+    @graph = Graph.new(graph_params)
+    if @graph.save
+      flash[:success] = 'Graph successfully created'
+      redirect_to graphs_path
+    else
+      flash[:error] = 'Something went wrong'
+      render 'new'
+    end
+  end
+
+  private
+
+  def graph_params
+    params.require(:graph)
+          .permit(:name, :property)
+  end
 end
