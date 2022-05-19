fd64b110acc694152aae826e474fd35ebfe56927
Yan Siegers <hello@yansiegers.nl>
2022-05-19 16:28:45 +0200

    Add editing of graphs

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class GraphsController < ApplicationController
+  before_action :find_graph, only: %i[edit update destroy]
+  before_action :get_properties, only: %i[new edit]
+
   def index
     @graphs = Graph.all
   end
@@ -20,8 +23,19 @@ class GraphsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @graph.update(graph_params)
+      flash[:success] = 'Graph was successfully updated'
+      redirect_to graphs_path
+    else
+      flash[:error] = 'Something went wrong'
+      render 'edit'
+    end
+  end
+
   def destroy
-    @graph = Graph.find(params[:id])
     if @graph.destroy
       flash[:success] = 'Graph was successfully deleted'
       redirect_to graphs_path
@@ -31,11 +45,18 @@ class GraphsController < ApplicationController
     end
   end
 
-
   private
 
   def graph_params
     params.require(:graph)
           .permit(:name, :property)
+  end
+
+  def get_properties
+    @properties = MeasuringPoint.pluck(:property).uniq
+  end
+
+  def find_graph
+    @graph = Graph.find(params[:id])
   end
 end
