64328eb87987080c61228e7a52344888bbaf94b2
Yan Siegers <hello@yansiegers.nl>
2022-06-01 12:00:38 +0200

    Add creation of bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,4 +4,27 @@ class BookmarksController < ApplicationController
   def index
     @bookmarks = Bookmark.all
   end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      flash[:success] = 'Bookmark successfully created'
+      redirect_to bookmarks_path
+    else
+      flash[:error] = 'Something went wrong'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark)
+          .permit(:name, :favorite, :user_id)
+  end
 end
--- app/helpers/system/users_helper.rb
+++
@@ -1,2 +0,0 @@
-module System::UsersHelper
-end
