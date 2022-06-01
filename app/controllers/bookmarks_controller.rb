b2d945c9ad0537a04d4a32af2b6638fac2f7d3ec
Yan Siegers <hello@yansiegers.nl>
2022-06-01 12:10:17 +0200

    Add editing of bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class BookmarksController < ApplicationController
+  before_action :find_bookmark, only: %i[edit update destroy]
+
   def index
     @bookmarks = Bookmark.all
   end
@@ -20,11 +22,26 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @bookmark.update(bookmark_params)
+      flash[:success] = 'Bookmark was successfully updated'
+      redirect_to bookmarks_path
+    else
+      flash[:error] = 'Something went wrong'
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   private
 
   def bookmark_params
     params.require(:bookmark)
           .permit(:name, :favorite, :user_id)
+  end
+
+  def find_bookmark
+    @bookmark = Bookmark.find(params[:id])
   end
 end
