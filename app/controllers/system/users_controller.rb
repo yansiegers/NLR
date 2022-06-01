fa99c2edb9431cf42fb31cd93875cb2bfc248585
Yan Siegers <hello@yansiegers.nl>
2022-06-01 10:55:34 +0200

    Add creation of users

--- app/controllers/system/users_controller.rb
+++ app/controllers/system/users_controller.rb
@@ -4,4 +4,26 @@ class System::UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'User successfully created'
+      redirect_to system_users_path
+    else
+      flash[:error] = 'Something went wrong'
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+          .permit(:full_name, :email_address, :password)
+  end
 end
