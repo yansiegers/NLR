f254dca4a0ccbe86c578d75f67e407b0dafe683f
Yan Siegers <hello@yansiegers.nl>
2022-06-09 14:10:37 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,31 @@ puts 'Seeding...'
 
 user = User.last # created in user db migration
 
-bookmark1 = user.bookmarks.create(name: 'Weight & balance', favorite: false)
-bookmark2 = user.bookmarks.create(name: 'Temperature', favorite: false)
-bookmark3 = user.bookmarks.create(name: 'Speed', favorite: false)
+graph_category1 = GraphCategory.create(name: 'Weight & balance')
+graph_category2 = GraphCategory.create(name: 'Temperature')
+graph_category3 = GraphCategory.create(name: 'Speed')
 
-bookmark1.graphs << Graph.create([
+graph_category1.graphs << Graph.create([
   { name: 'Weight', property: 'Weight' },
   { name: 'Center of gravity deviation Y', property: 'COG_dev_y' }
 ])
-bookmark2.graphs << Graph.create([
+graph_category2.graphs << Graph.create([
   { name: 'Temperature front left', property: 'temp_front_left' },
   { name: 'Temperature front right', property: 'temp_front_right' }
 ])
-bookmark3.graphs << Graph.create(name: 'Pitot tube', property: 'pitot_tube')
+graph_category3.graphs << Graph.create(name: 'Pitot tube', property: 'pitot_tube')
+
+workspace1 = user.workspaces.create(name: 'Workspace 1', favorite: false)
+workspace2 = user.workspaces.create(name: 'Workspace 2', favorite: false)
+workspace3 = user.workspaces.create(name: 'Workspace 3', favorite: false)
+
+workspace1.graphs << graph_category1.graphs
+workspace1.graphs << graph_category2.graphs
+
+workspace2.graphs << graph_category2.graphs
+workspace2.graphs << graph_category3.graphs
+
+workspace3.graphs << graph_category3.graphs
+workspace3.graphs << graph_category1.graphs
 
 puts 'Seeding done.'
